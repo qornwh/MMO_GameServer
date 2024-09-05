bc0ced06ceb1c2057389e7fa08b9f45671872bdc
qornwh <qornwh@gmail.com>
2024-09-05 22:54:46 +0900

    유저 접송정보 체크 기능 추가

---
+++ GameServer/GameUserAccess.cpp
@@ -0,0 +1,43 @@
+﻿#include "GameUserAccess.h"
+
+void GameUserAccess::ClearUser()
+{
+    _userAccess.clear();
+    _playerAccess.clear();
+}
+
+bool GameUserAccess::AccessUser(int32 userCode, int32 sessionId)
+{
+    // sessionId : 들어온 세션 아이디
+    // userCode : db상 unique 계정 번호
+    if (_userAccess.find(userCode) != _userAccess.end())
+    {
+        if (_userAccess[userCode] > 0)
+            return false;
+    }
+    _userAccess[userCode] = sessionId;
+    return true;
+}
+
+void GameUserAccess::ReleaseUser(int32 userCode)
+{
+    _userAccess[userCode] = -1;
+}
+
+bool GameUserAccess::AccessPlayer(int32 playerCode, int32 sessionId)
+{
+    // sessionId : 들어온 세션 아이디
+    // playerCode : db상 unique 캐릭터 번호
+    if (_playerAccess.find(playerCode) != _playerAccess.end())
+    {
+        if (_playerAccess[playerCode] > 0)
+            return false;
+    }
+    _playerAccess[playerCode] = sessionId;
+    return true;
+}
+
+void GameUserAccess::ReleasePlayer(int32 playerCode)
+{
+    _playerAccess[playerCode] = -1;
+}
---
+++ GameServer/GameUserAccess.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include "Types.h"
+#include "ReadWriteLock.h"
+
+class GameUserAccess : public std::enable_shared_from_this<GameUserAccess>
+{
+public:
+    void ClearUser();
+    bool AccessUser(int32 userCode, int32 sessionId);
+    void ReleaseUser(int32 userCode);
+    bool AccessPlayer(int32 playerCode, int32 sessionId);
+    void ReleasePlayer(int32 playerCode);
+
+private:
+    Lock lock;
+    Map<int32, int32> _userAccess;
+    Map<int32, int32> _playerAccess;
+};
